1e4f0092cceee324b82e55c6123c27f55cb64be8
acc0mplish <70960296+acc0mplish@users.noreply.github.com>
2025-06-04 16:44:30 +0900

    Update views_test.go

--- gcal/views_test.go
+++ gcal/views_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/api/calendar/v3"
 
-	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/remote"
+	"github.com/acc0mplish/klic-mattermost-plugin-mscalendar/calendar/remote"
 )
 
 func createMinimalCalendarEvent() calendar.Event {
